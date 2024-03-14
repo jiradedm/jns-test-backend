853ef3088cd85da8862fcd925607fb40aa874272
Jiraded <jiraded27@gmail.com>
2024-03-14 18:43:20 +0700

    add place route

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,28 @@
+import axios from "axios";
 import express from "express";
 
 const app = express();
 
-app.get("/", (_, res) => {
-  return res.send("Hello World");
+app.get("/:placeName", async (req, res, next) => {
+  const { placeName } = req.params;
+
+  try {
+    const resp = await axios.post(
+      "https://places.googleapis.com/v1/places:searchText",
+      { textQuery: placeName },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "X-Goog-Api-Key": "AIzaSyClulrZcf-CYhUdzT_dWwpzCJDoOUSmCmg",
+          "X-Goog-FieldMask": "places.displayName,places.formattedAddress",
+        },
+      },
+    );
+
+    return res.send(resp.data);
+  } catch (err) {
+    return next(err);
+  }
 });
 
 const run = () => {
